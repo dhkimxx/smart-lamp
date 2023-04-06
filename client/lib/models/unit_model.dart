6c1865992712e6a38551399671e172d43bc9f0a3
dhkimxx <dhkimxx@naver.com>
2023-04-06 18:08:45 +0900

    distance widget

--- client/lib/models/unit_model.dart
+++ client/lib/models/unit_model.dart
@@ -1,1 +1,9 @@
-class UnitModel {}
+class UnitModel {
+  final String name;
+  final int distance;
+  final int time;
+  UnitModel.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        distance = json['distance'],
+        time = json['time'];
+}
--- client/lib/screens/unit_detail_screen.dart
+++ client/lib/screens/unit_detail_screen.dart
@@ -11,11 +11,14 @@ class UnitDetailScreen extends StatelessWidget {
   });
   var myMqttClient = MyMqttClient();
 
+  var distance = 50;
+
   @override
   Widget build(BuildContext context) {
     myMqttClient.connect();
     return Scaffold(
       appBar: AppBar(
+        elevation: 5,
         backgroundColor: Colors.white,
         foregroundColor: Colors.blue,
         title: Text(
@@ -26,44 +29,93 @@ class UnitDetailScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: Colors.blue,
+                    )),
+                child: Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        myMqttClient.pubMessage(
+                          unitCode,
+                          'ON',
+                        );
+                      },
+                      child: const Text('ON'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        myMqttClient.pubMessage(
+                          unitCode,
+                          'OFF',
+                        );
+                      },
+                      child: const Text('OFF'),
+                    ),
+                  ],
+                ),
               ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: Colors.blue,
-                  )),
-              child: Row(
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      myMqttClient.pubMessage(
-                        unitCode,
-                        'ON',
-                      );
-                    },
-                    child: const Text('ON'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      myMqttClient.pubMessage(
-                        unitCode,
-                        'OFF',
-                      );
-                    },
-                    child: const Text('OFF'),
-                  ),
-                ],
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '감지 거리(cm):  ',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                ),
               ),
-            ),
-          ],
-        ),
+              const SizedBox(
+                width: 5,
+              ),
+              SizedBox(
+                width: 50,
+                height: 48,
+                child: TextFormField(
+                  autofocus: false,
+                  initialValue: '$distance',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue),
+                child: TextButton(
+                  onPressed: () {
+                    print(distance);
+                  },
+                  child: const Text(
+                    '확인',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
