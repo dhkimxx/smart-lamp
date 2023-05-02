439bea7a640ef1ee2e1d4ac30bac614ccbbf52e9
dhkimxx <dhkimxx@naver.com>
2023-05-02 23:35:03 +0900

    join screen

--- client/lib/screens/join_screen.dart
+++ client/lib/screens/join_screen.dart
@@ -1,22 +1,26 @@
+import 'package:client/screens/home_screen.dart';
 import 'package:client/service/join_service.dart';
+import 'package:client/widgets/alter_dialog_widget.dart';
 import 'package:flutter/material.dart';
 
-class TestScreen extends StatefulWidget {
-  const TestScreen({super.key});
+class JoinScreen extends StatefulWidget {
+  const JoinScreen({super.key});
 
   @override
-  _TestScreenState createState() => _TestScreenState();
+  _JoinScreenState createState() => _JoinScreenState();
 }
 
-class _TestScreenState extends State<TestScreen> {
+class _JoinScreenState extends State<JoinScreen> {
   final _formKey = GlobalKey<FormState>();
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _passwordCheckController = TextEditingController();
 
   @override
   void dispose() {
     _usernameController.dispose();
     _passwordController.dispose();
+    _passwordCheckController.dispose();
     super.dispose();
   }
 
@@ -24,16 +28,35 @@ class _TestScreenState extends State<TestScreen> {
     if (_formKey.currentState!.validate()) {
       final userId = _usernameController.text;
       final userPw = _passwordController.text;
+      final userPwCheck = _passwordCheckController.text;
+
+      if (userPw != userPwCheck) {
+        alterDialog(
+          context: context,
+          title: '오류',
+          contents: '비밀번호을 확인하세요.',
+        );
+        return;
+      }
 
       try {
         print("$userId, $userPw");
         final token = await joinUser(userId, userPw);
+        _navigateToHomeScreen();
         // 로그인 성공 시 처리할 코드 작성
       } catch (e) {
         // 로그인 실패 시 처리할 코드 작성
         print("회원가입 실패");
       }
     }
+  }
+
+  void _navigateToHomeScreen() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ),
+    );
   }
 
   @override
@@ -69,7 +92,7 @@ class _TestScreenState extends State<TestScreen> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  hintText: 'Username',
+                  hintText: '아이디',
                 ),
               ),
               TextFormField(
@@ -82,7 +105,20 @@ class _TestScreenState extends State<TestScreen> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  hintText: 'Password',
+                  hintText: '비밀번호',
+                ),
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  hintText: '비밀번호 확인',
                 ),
               ),
               const SizedBox(height: 16.0),
