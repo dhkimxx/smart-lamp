921c5fa7fb95587a23f3aedc8919a236d5c94b1d
dhkimxx <dhkimxx@naver.com>
2023-10-02 15:24:35 +0900

    .

--- unit/smartLampUnit.ino
+++ unit/smartLampUnit.ino
@@ -2,29 +2,29 @@
 #include <ESP8266WiFiMulti.h>
 #include <PubSubClient.h>
 #include <ESP8266HTTPClient.h>
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
+
+const int LED = 2; //D4
+const int TRIG = 16; //D0
+const int ECHO = 15; //D8
+const int PIR = 5; //D1
 
 const char* ssid = "myhomewifi2.4g";
-//const char* ssid = "kdh";
 const char* password = "wifi82825535";
-const char* mqtt_server = "18.214.206.169";
-const int port = 1883;
+const char* mqtt_server = "13.209.242.126";
+const int port = 54305;
 
 String clientId = "100";
 String topicOrder = clientId;
 String topicSetDistance = "setDistance/" + clientId;
 String topicSetTime = "setTime/" + clientId;
 String topicSetBrightness = "setBrightness/" + clientId;
+String topicSetWarningTime = "setWarningTime" + clientId;
 
-int Distance = 50;  // default detection distance 50cm
-int Time = 10000;  // default led on time 10sec (10000msec)
+int Distance = 50;  // Default detection distance: 50cm
+int Time = 10000;  // Default led on time: 10sec (10000msec)
 float Brightness = 1; // Brightness scope: 0~10 stemp, analogWrite: 1023 * (1 - Brightness / 10)
-int WarningTime = 5;
-
-const int LED = 2; //D4
-const int TRIG = 16; //D0
-const int ECHO = 15; //D8
-const int PIR = 5; //D1
+int WarningTime = 10;   // Default detecting motionless time: 5sec
 
 unsigned long loop_time_previous = 0;
 unsigned long check_time_previous = 0;
@@ -32,23 +32,21 @@ unsigned long check_time = 500;
 unsigned long warning_time_previous = 0;
 unsigned long warning_time = WarningTime * 1000;
 
-ESP8266WiFiMulti WiFiMulti;
-WiFiClient espClient;
-PubSubClient client(espClient);
-String str = "a";
-
-unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
 bool delay_flag = false;
 bool check_flag = false;
 bool warning_flag = false;
 
+ESP8266WiFiMulti WiFiMulti;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void http_request(){
   WiFiClient client;
   HTTPClient http;
   Serial.print("[HTTP] begin...\n");
-    if (http.begin(client, "http://18.214.206.169:8080/send-test")) {  // HTTP
+    if (http.begin(client, "https://tuk-smartlamp.run.goorm.site/send-one")) {  // HTTP
       Serial.print("[HTTP] POST...\n");
       // start connection and send HTTP header
       http.addHeader("Content-Type", "application/json");
@@ -153,13 +151,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
       analogWrite(LED, 1023 * (1 - Brightness / 10));
     }
   }
+  else if(!strcmp(topic, topicSetWarningTime.c_str())){
+    WarningTime = atoi(messageTemp.c_str());
+    Serial.print("set WarningTime: ");
+    Serial.println(WarningTime);
+  }
 }
 
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     
-    if (client.connect(str.c_str())) {
+    if (client.connect("")) {
       Serial.println("connected");
       client.publish("login", clientId.c_str());
       client.subscribe(topicOrder.c_str());
@@ -177,8 +180,6 @@ void reconnect() {
         delay(1000);
       }
       Serial.println(")");
-      str = str + "a";
-      Serial.println(str);
     }
   }
 }
